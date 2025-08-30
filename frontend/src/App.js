f6718ecb1a47d019e131f4925a68d40705446802
kst-01 <proksttech@gmail.com>
2025-08-30 19:40:06 +0530

    added a new logo and updated the title

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,7 +122,6 @@ function App() {
   function MainContent() {
     return (
       <main className="main-layout">
-      <head><title>MinLive</title></head>
         <div className="box large-box">
           <h2 className="box-title">Live Chat :</h2>
           <div className="inner-box">
